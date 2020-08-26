17596882f63fc33105ac7a7e9285d3ae1d8e11ba
srushtee <srushtees88@gmail.com>
2020-08-27 01:00:09 +0530

    commit hell

---
+++ src/Pages/HomePage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './homepage.css'
+
+import Directory from '../../Components/Directory/Directory'
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default Homepage;
